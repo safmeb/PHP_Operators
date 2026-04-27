02da7617946cda05f66a173279ae6e0e93cc2e65
Artem_Safon <coruscardea@gmail.com>
2026-04-27 22:16:15 +0300

    index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ echo "Имя:" . PHP_EOL;
     $name = mb_convert_case($name, MB_CASE_TITLE, "UTF-8");// приводим первый символ к верхнему регистру
 echo "Фамилия:" . PHP_EOL;
    $surname = trim(fgets(STDIN)); // вводим фамилию пользователя
-if (empty($surname)) {// проверка на пустой ввод имени
+if (empty($surname)) {
         fwrite(STDERR, "Введите фамилию");
         exit(1);
     }
@@ -16,15 +16,14 @@ if (empty($surname)) {// проверка на пустой ввод имени
 echo "Отчество:" . PHP_EOL;
     $patronymic = trim(fgets(STDIN)); // вводим отчество пользователя
     $patronymic = mb_convert_case($patronymic, MB_CASE_TITLE, "UTF-8");// пусть будет сразу с верхним регистром
-   if (empty($patronymic)) { // допускаю что можно ввести пустое отчество, тогда по разному выводим $fullName и  $surnameAndInitials
-    $fullName = $surname.' '.$name;
-    $surnameAndInitials = $surname .' '.  mb_substr($name, 0, 1, 'UTF-8').'.';
-    }else {
+   if (empty($patronymic)) {
+    fwrite(STDERR, "Введите отчество");
+        exit(1);}
+
     $fullName = $surname.' '.$name.' '. $patronymic; 
     $surnameAndInitials = $surname .' '.  mb_substr($name, 0, 1, 'UTF-8').'.'.mb_substr($patronymic, 0, 1, 'UTF-8').'.';
-    }
-
+    
 echo "Полное имя: "."'". $fullName."'", PHP_EOL;
 $fio = mb_substr($surname, 0, 1, 'UTF-8') . mb_substr($name, 0, 1, 'UTF-8') . mb_substr($patronymic, 0, 1, 'UTF-8');
 echo "Фамилия и инициалы: "."'". $surnameAndInitials."'", PHP_EOL; 
-echo "Аббревиатура: "."'". $fio."'", PHP_EOL; 
+echo "Аббревиатура: "."'". $fio."'", PHP_EOL;  
